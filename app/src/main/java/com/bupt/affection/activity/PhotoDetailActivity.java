b01c3937ff857e134e4523dba8a4da0af263c61b
406676675@qq.com <406676675@qq.com>
2016-07-20 21:44:28 +0800

    add PhotoDetail.Activity

---
+++ app/src/main/java/com/bupt/affection/activity/PhotoDetailActivity.java
@@ -0,0 +1,15 @@
+package com.bupt.affection.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bupt.affection.R;
+
+public class PhotoDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detail);
+    }
+}
