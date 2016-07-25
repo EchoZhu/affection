a0bad4dc43bd58609c62322095033660f6c363f4
406676675@qq.com <406676675@qq.com>
2016-07-25 23:52:38 +0800

    finish activity_settings

---
+++ app/src/main/java/com/bupt/affection/activity/SetActivity.java
@@ -0,0 +1,15 @@
+package com.bupt.affection.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bupt.affection.R;
+
+public class SetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set);
+    }
+}
